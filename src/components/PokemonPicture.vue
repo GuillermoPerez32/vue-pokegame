b5a30211bc006aaf5b67b4ddb62b44687c50710b
Luis Guillermo Pérez Rodríguez <86862509+GuillermoPerez32@users.noreply.github.com>
2022-09-21 07:12:45 -0600

    added some properties

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,43 @@
+<template>
+    <img class="hidden-pokemon" :src="imgSrc" alt="" v-if="!is_hidden">
+    <img class="" :src="imgSrc" alt="" v-if="is_hidden">
+</template>
+
+<script>
+export default {
+
+    props: {
+        pokemonId: {
+            type: Number,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            is_hidden: false,
+        }
+    },
+    computed: {
+        imgSrc() {
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+        },
+    }
+}
+</script>
+
+<style scoped>
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>¿Quién es este pokemón?</h1>
+  <PokemonPicture :pokemonId="500"/>
+  <PokemonOptions/>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
+export default {
+    components: { PokemonPicture, PokemonOptions }
+}
+</script>
+
+<style>
+
+</style>
